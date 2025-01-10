0b309ab7bca435572ac751f226deb964a80a97dc
Sohangkumar Patel <71547113+samsamurai301@users.noreply.github.com>
2025-01-10 17:54:24 +0100

    Add ECharts integration with a new Chart component and sample chart data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,36 @@
 
 import React from 'react';
 import Navbar from './components/Navbar';
+import Chart from './components/Chart';
 import { Box, Typography } from '@mui/material';
 
 const Home: React.FC = () => {
+  const sampleOption = {
+    title: {
+      text: 'Stunning UI Template - Sample Chart',
+    },
+    tooltip: {
+      trigger: 'axis',
+    },
+    xAxis: {
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    },
+    yAxis: {
+      type: 'value',
+    },
+    series: [
+      {
+        name: 'Sales',
+        type: 'line',
+        data: [120, 200, 150, 80, 70, 110, 130],
+        smooth: true,
+        lineStyle: {
+          width: 3,
+        },
+      },
+    ],
+  };
   return (
     <Box sx={{ minHeight: '100vh', bgcolor: 'background.default', color: 'text.primary' }}>
       <Navbar />
@@ -16,6 +43,9 @@ const Home: React.FC = () => {
         <Typography variant="body1">
           This is a fully customizable and optimized UI template using Next.js, Redux, and MUI.
         </Typography>
+      </Box>
+      <Box sx={{ p: 3 }}>
+      <Chart option={sampleOption} />
       </Box>
     </Box>
   );
